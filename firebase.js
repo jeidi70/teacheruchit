ef5ba9229ff65ae9af7b45246853c83b63fff652
jeidi70 <ilyaasmirnov02@gmail.com>
2025-06-12 05:23:34 +0300

    бновлена главная страница: добавлены SVG-иллюстрации, улучшен дизайн, добавлена адаптивность

--- firebase.js
+++ firebase.js
@@ -6,13 +6,29 @@ const firebaseConfig = {
   storageBucket: "teacheruchit.firebasestorage.app",
   messagingSenderId: "762586190395",
   appId: "1:762586190395:web:78333e7d5fa3dd6dd1021e",
-  measurementId: "G-K0BHLKHNHF"
+  measurementId: "G-K0BHLKHNHH"
 };
 
 // Инициализация Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
 
+// Инициализация сервисов
 const auth = firebase.auth();
 const db = firebase.firestore();
+
+// Настройка языка для Firebase Auth
+auth.useDeviceLanguage();
+
+// Настройка персистентности Firestore
+db.enablePersistence()
+  .catch((err) => {
+    if (err.code === 'failed-precondition') {
+      console.log('Multiple tabs open, persistence can only be enabled in one tab at a time.');
+    } else if (err.code === 'unimplemented') {
+      console.log('The current browser does not support persistence.');
+    }
+  });
+
+// Экспорт для использования в других файлах
+window.auth = auth;
+window.db = db;
